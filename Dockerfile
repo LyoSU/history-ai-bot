76c48b7cd5ddb370c08dee1a3247787b16858eaf
Vladislav Kamenev <mazahakaforever@ya.ru>
2019-12-25 14:43:40 +0300

    Docker, inline Mode, etc (#1)
    * Fixed .env.example. Added Dockerfile.
    
    * Moved to fetch. Added inline mode.
    
    * Inline query reworked.

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:alpine
+
+ENV NODE_WORKDIR /home/node/app
+
+WORKDIR $NODE_WORKDIR
+ADD . $NODE_WORKDIR
+
+RUN npm install
+
+CMD ["npm", "start"]
--- bot.js
+++ bot.js
@@ -1,52 +1,126 @@
 const Telegraf = require('telegraf')
-const got = require('got')
+const Extra = require('telegraf/extra')
+const fetch = require('node-fetch')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const API_LINK = 'https://models.dobro.ai/gpt2/medium/'
+const FETCH_DEFAULT = {
+  timeout: 10000,
+  headers: { 'Content-Type': 'application/json' },
+  method: 'POST'
+}
 
-bot.use(async (ctx, next) => {
-  ctx.ms = new Date()
-  next()
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+  telegram: {
+    apiRoot: 'http://51.15.86.205:8012'
+  }
 })
-
 bot.use(async (ctx, next) => {
+  const before = new Date()
   await next(ctx)
-
-  const ms = new Date() - ctx.ms
+  const ms = new Date() - before
 
   console.log('Response time %sms', ms)
 })
 
-bot.start(async (ctx, next) => {
+bot.start(async (ctx) => {
   ctx.replyWithHTML('Привет, я дополню твою историю. Нужно лишь написать её начало из нескольких предложений. Чем четче будет сформулировано начало, тем лучше будет результат.\n\n<b>GitHub бота:</b> github.com/LyoSU/history-ai-bot\n\nБот это лишь "обертка" для взаимодействия с API. Авторство принадлежит оригинальному автору проекта и все благодарности необходимо отправлять ему.\n\nGitHub проекта: github.com/mgrankin/ru_transformers\nВеб-версия проекта: text.skynet.center\n\n<b>Блог разработчика:</b> @LyBlog')
 })
 
-bot.use(async (ctx, next) => {
-  const extend = got.extend({
-    responseType: 'json',
-    timeout: 10000,
-    throwHttpErrors: false
-  })
+const apiRequest = async (text, samples) => {
+  const params = {
+    prompt: text,
+    length: 60,
+    num_samples: samples
+  }
 
+  const response = await fetch(API_LINK, {
+    ...FETCH_DEFAULT,
+    body: JSON.stringify(params)
+  })
+  return response.json()
+}
+
+bot.on('message', async (ctx) => {
   if (ctx.message.text) {
     ctx.replyWithChatAction('typing')
     const text = ctx.message.text
 
-    const mediumResult = await extend.post('https://models.dobro.ai/gpt2/medium/', {
-      json: {
-        prompt: text,
-        length: 60,
-        num_samples: 1
-      }
-    })
+    try {
+      const { replies } = await apiRequest(text, 1)
+      const answer = `<i>${text}</i>${replies.join('')}`
 
-    if (mediumResult.body && mediumResult.body.replies && mediumResult.body.replies.length > 0) {
-      const result = `<i>${text}</i>${mediumResult.body.replies[0]}`
-
-      ctx.replyWithHTML(result, {
+      await ctx.replyWithHTML(answer, {
         reply_to_message_id: ctx.message.message_id
       })
+    } catch (e) {
+
     }
   }
+})
+
+bot.on('inline_query', async (ctx) => {
+  const { inlineQuery, answerInlineQuery } = ctx
+  if (inlineQuery.query) {
+    try {
+      return answerInlineQuery(
+        [{
+          type: 'article',
+          id: +(new Date()),
+          title: 'Сгенерировать историю!',
+          description: 'Нажми чтобы сгенерировать историю!',
+          input_message_content: {
+            message_text: '<i>Генерирую историю...</i>',
+            parse_mode: 'HTML'
+          },
+          reply_markup: {
+            inline_keyboard: [
+              [
+                { text: '⏳ Генерируется история', callback_data: 'upld' }
+              ]
+            ]
+          }
+        }],
+        {
+          is_personal: true,
+          cache_time: 0
+        }
+      )
+    } catch (e) {
+      return answerInlineQuery(
+        [
+          {
+            type: 'article',
+            id: +new Date(),
+            title: 'Ошибка',
+            description: 'Не могу сгенерировать историю',
+            input_message_content: {
+              message_text: 'Не могу сгенерировать историю'
+            }
+          }
+        ],
+        {
+          is_personal: true,
+          cache_time: 0
+        }
+      )
+    }
+  }
+})
+
+bot.on('chosen_inline_result', async (ctx) => {
+  const { chosenInlineResult } = ctx;
+  const { query, inline_message_id } = chosenInlineResult;
+  if (query && inline_message_id) {
+    try {
+      const { replies } = await apiRequest(query, 1)
+      const answer = `<i>${query}</i>${replies.join('')}`
+
+      await ctx.telegram.editMessageText(null, null, inline_message_id, answer, Extra.HTML())
+    } catch (e) {
+      await ctx.telegram.editMessageText(null, null, inline_message_id, 'Не могу сгенерировать историю');
+    }
+  }
+  console.log('chosen inline result', chosenInlineResult);
 })
 
 bot.catch((err, ctx) => {
